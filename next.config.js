814fbb4bbbe5b4064009cbe5b71f96be1173aef9
reminder2k1 <reminder2k1@gmail.com>
2023-01-06 19:35:51 +0700

    first commit

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  basePath: '/next-deploy',
+  exportPathMap: "https://github.com/reminder2k1/next-deploy"
+}
+
+module.exports = nextConfig
